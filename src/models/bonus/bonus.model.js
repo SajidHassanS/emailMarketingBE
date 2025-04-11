c736a40c1cce7aef96977684f9c798fd5ecf7029
SajidHassanAgro <sajid.hassan@agronomics.pk>
2025-04-11 14:57:10 +0500

    1adddddd

--- src/models/bonus/bonus.model.js
+++ src/models/bonus/bonus.model.js
@@ -3,45 +3,45 @@ import sequelize from "../../config/dbConfig.js";
 import User from "../user/user.model.js";
 
 const Bonus = sequelize.define(
-    "Bonus",
-    {
-        uuid: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-        },
-        userUuid: {
-            type: DataTypes.UUID,
-            allowNull: false,
-            references: { model: 'Users', key: 'uuid' },
-            onDelete: 'CASCADE',
-        },
-        type: {
-            type: DataTypes.ENUM('signup', 'referral'),
-            allowNull: false,
-        },
-        amount: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        unlockedAfterFirstWithdrawal: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-        },
-        refereeUuid: {
-            type: DataTypes.UUID,
-            allowNull: true,
-            references: {
-                model: User,
-                key: 'uuid',
-            },
-        }
+  "Bonus",
+  {
+    uuid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
-    {
-        schema: "public",
-        timestamps: true,
-        underscored: true,
-    }
+    // userUuid: {
+    //     type: DataTypes.UUID,
+    //     allowNull: false,
+    //     references: { model: 'Users', key: 'uuid' },
+    //     onDelete: 'CASCADE',
+    // },
+    type: {
+      type: DataTypes.ENUM("signup", "referral"),
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    unlockedAfterFirstWithdrawal: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    refereeUuid: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      references: {
+        model: User,
+        key: "uuid",
+      },
+    },
+  },
+  {
+    schema: "public",
+    timestamps: true,
+    underscored: true,
+  }
 );
 
 export default Bonus;
@@ -50,18 +50,18 @@ export default Bonus;
 
 // Define associations
 User.hasMany(Bonus, {
-    foreignKey: 'userUuid',
-    as: 'bonuses',
-    onDelete: 'CASCADE',
+  foreignKey: "userUuid",
+  as: "bonuses",
+  onDelete: "CASCADE",
 });
 
 Bonus.belongsTo(User, {
-    foreignKey: 'userUuid',
-    as: 'user',
+  foreignKey: "userUuid",
+  as: "user",
 });
 
 // relationship with the referee (the user who used the referral code)
 Bonus.belongsTo(User, {
-    foreignKey: 'refereeUuid',
-    as: 'referee',
+  foreignKey: "refereeUuid",
+  as: "referee",
 });
