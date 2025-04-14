7b73fb4a7ff2dc950ac4f3d65164f5bdfea763fe
SajidHassanAgro <sajid.hassan@agronomics.pk>
2025-04-14 11:58:58 +0500

    change

--- src/models/withdrawal/bonusWithdrawal.model.js
+++ src/models/withdrawal/bonusWithdrawal.model.js
@@ -4,46 +4,46 @@ import Bonus from "../withdrawal/bonus.model.js";
 import User from "../user/user.model.js";
 
 const BonusWithdrawal = sequelize.define(
-    "BonusWithdrawal",
-    {
-        uuid: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-        },
-        bonusUuid: {
-            type: DataTypes.UUID,
-            allowNull: false,
-            references: {
-                model: "Bonuses",
-                key: "uuid",
-            },
-            onDelete: "CASCADE",
-        },
-        userUuid: {
-            type: DataTypes.UUID,
-            allowNull: false,
-            references: {
-                model: "Users",
-                key: "uuid",
-            },
-            onDelete: "CASCADE",
-        },
-        withdrawalMethodUuid: {
-            type: DataTypes.UUID,
-            allowNull: false,
-            references: { model: "WithdrawalMethod", key: "uuid" },
-        },
-        status: {
-            type: DataTypes.ENUM('pending', 'approved', 'withdrawn'),
-            defaultValue: 'pending',
-        },
+  "BonusWithdrawal",
+  {
+    uuid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
-    {
-        schema: "public",
-        timestamps: true,
-        underscored: true,
-    }
+    bonusUuid: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "Bonuses",
+        key: "uuid",
+      },
+      onDelete: "CASCADE",
+    },
+    userUuid: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "Users",
+        key: "uuid",
+      },
+      onDelete: "CASCADE",
+    },
+    withdrawalMethodUuid: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: { model: "WithdrawalMethod", key: "uuid" },
+    },
+    status: {
+      type: DataTypes.ENUM("pending", "approved", "withdrawn"),
+      defaultValue: "pending",
+    },
+  },
+  {
+    schema: "public",
+    timestamps: true,
+    underscored: true,
+  }
 );
 
 export default BonusWithdrawal;
@@ -51,22 +51,22 @@ export default BonusWithdrawal;
 // ===================== Associations ======================
 
 Bonus.hasOne(BonusWithdrawal, {
-    foreignKey: "bonusUuid",
-    as: "withdrawal",
-    onDelete: "CASCADE",
+  foreignKey: "bonusUuid",
+  as: "withdrawal",
+  onDelete: "CASCADE",
 });
 
 BonusWithdrawal.belongsTo(Bonus, {
-    foreignKey: "bonusUuid",
-    as: "bonus",
+  foreignKey: "bonusUuid",
+  as: "bonus",
 });
 
 User.hasMany(BonusWithdrawal, {
-    foreignKey: "userUuid",
-    as: "bonusWithdrawals",
+  foreignKey: "userUuid",
+  as: "bonusWithdrawals",
 });
 
 BonusWithdrawal.belongsTo(User, {
-    foreignKey: "userUuid",
-    as: "user",
+  foreignKey: "userUuid",
+  as: "user",
 });
